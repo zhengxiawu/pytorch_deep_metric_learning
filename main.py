5b922f724907ea2007798c2bee80ba0b211d4def
sherwood-zheng <zhengxiawu@126.com>
2018-01-28 22:16:15 +0800

    add softmax_train

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import models
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
-parser.add_argument('--arch', '-a', default='resnet18',help='chose a network')
+parser.add_argument('--arch', '-a', default='resnet50',help='chose a network')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--epochs', default=90, type=int, metavar='N',
--- models/resnet-50.py
+++ models/resnet-50.py
@@ -2,11 +2,12 @@ import torch.nn as nn
 import math
 import torch.utils.model_zoo as model_zoo
 import torch.nn.functional as F
+import torch
 
 __all__ = ['ResNet', 'resnet50']
 
 model_urls = {
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet50': '/home/zhengxiawu/project/pytorch_deep_metric_learning/pretrained_models/resnet50-19c8e357.pth',
 }
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -52,40 +53,10 @@ class Bottleneck(nn.Module):
 
         return out
 
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000):
+    def __init__(self, block, layers, num_class):
         self.inplanes = 64
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
@@ -97,9 +68,11 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.global_avg_pool =nn.AvgPool2d()
-        self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.avg_pool = nn.AvgPool2d(7, stride=1)
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.global_max_pool = nn.AdaptiveMaxPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_class)
+
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -137,26 +110,46 @@ class ResNet(nn.Module):
         x = self.layer3(x)
         x = self.layer4(x)
         return x
-    def forward(self, x):
+    def forward(self, x, **kwargs):
+        #**kwargs form
+        #pool_type:default max_avg, can be set max, avg
+        #scale: int default 128
+        #scda:True or false, if use saliency
+        #is_train
         x = self.extract_conv_feature(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        # feature norm
-        x = F.normalize(x,p=2,dim=1)
-        #scale
-        x = x * 128
+        if kwargs['scda']:
+            scda_x = torch.sum(x,1,keepdim=True)
+            mean_x = torch.mean(scda_x.view(scda_x.size(0),-1),1,True)
+            scda_x = scda_x - mean_x
+            scda_x = scda_x>0
+            scda_x.float()
+            x = x * scda_x
+        if kwargs['pool_type'] == 'max_avg':
+            avg_x = self.global_avg_pool(x)
+            avg_x = avg_x.view(avg_x.size(0), -1)
+            avg_x = F.normalize(avg_x,p=2,dim=1)
+            max_x = self.global_max_pool(x)
+            max_x = max_x.view(max_x.size(0), -1)
+            max_x = F.normalize(max_x,p=2,dim=1)
+            x = torch.cat((avg_x,max_x),dim=1)
+        x = x * kwargs['scale']
         # the last fc layer can be treat as distance compute
-        x = self.fc(x)
+        if kwargs['is_train']:
+            x = self.fc(x)
 
         return x
 
-def resnet50(pretrained=False, **kwargs):
+def resnet50(pretrained=True, num_class=100):
     """Constructs a ResNet-50 model.
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    model = ResNet(Bottleneck, [3, 4, 6, 3], num_class)
     if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+        pretrained_dict = torch.load(model_urls['resnet50'])
+        model_dict = model.state_dict()
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+        model_dict.update(pretrained_dict)
+        model.load_state_dict(model_dict)
     return model
